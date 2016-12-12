3f3bc63038e2c9058a9e9138bd500053a90b0682
layery <779487935@qq.com>
2016-12-12 17:51:18 +0800

    update

--- admin/views/layouts/admin.php
+++ admin/views/layouts/admin.php
@@ -2,38 +2,38 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Layery admin</title>
-    <style type="text/css">
-        .layout {
-            border: red solid 2px;
-            height:900px;
-            width:100%;
-        }
-        .layout-left {
-            float: left;
-            width: 25%;
-            height: auto;
-            border: green solid 1px;
-        }
-        .layout-center {
-            float: right;
-            width: 73%;
-            height: auto;
-            border: black solid 1px;
-        }
-    </style>
+    <title>Basic Layout - jQuery EasyUI Demo</title>
+    <link rel="stylesheet" type="text/css" href="../../themes/default/easyui.css">
+    <link rel="stylesheet" type="text/css" href="../../themes/icon.css">
+    <link rel="stylesheet" type="text/css" href="../demo.css">
+    <script type="text/javascript" src="../../jquery.min.js"></script>
+    <script type="text/javascript" src="../../jquery.easyui.min.js"></script>
 </head>
 <body>
-<div id="layout" class="layout">
-    <div class="layout-top">
-        <?php $this->render('/common/top'); ?>
-    </div>
-    <div class="layout-left">
-        <?php $this->render('/common/left'); ?>
-    </div>
-    <div class="layout-center">
-        <?php echo $content;?>
+<h2>Basic Layout</h2>
+<p>The layout contains north,south,west,east and center regions.</p>
+<div style="margin:20px 0;"></div>
+<div class="easyui-layout" style="width:700px;height:350px;">
+    <div data-options="region:'north'" style="height:50px"></div>
+    <div data-options="region:'south',split:true" style="height:50px;"></div>
+    <div data-options="region:'east',split:true" title="East" style="width:100px;"></div>
+    <div data-options="region:'west',split:true" title="West" style="width:100px;"></div>
+    <div data-options="region:'center',title:'Main Title',iconCls:'icon-ok'">
+        <table class="easyui-datagrid"
+               data-options="url:'datagrid_data1.json',method:'get',border:false,singleSelect:true,fit:true,fitColumns:true">
+            <thead>
+            <tr>
+                <th data-options="field:'itemid'" width="80">Item ID</th>
+                <th data-options="field:'productid'" width="100">Product ID</th>
+                <th data-options="field:'listprice',align:'right'" width="80">List Price</th>
+                <th data-options="field:'unitcost',align:'right'" width="80">Unit Cost</th>
+                <th data-options="field:'attr1'" width="150">Attribute</th>
+                <th data-options="field:'status',align:'center'" width="60">Status</th>
+            </tr>
+            </thead>
+        </table>
     </div>
 </div>
+
 </body>
 </html>
--- admin/web/index.php
+++ admin/web/index.php
@@ -2,6 +2,7 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 define('IS_AJAX', isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
+define('JS_URL', '/web/js');
 
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
@@ -11,8 +12,7 @@ require(__DIR__ . '/../config/bootstrap.php');
 $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../../common/config/main.php'),
     require(__DIR__ . '/../../common/config/main-local.php'),
-    require(__DIR__ . '/../config/main.php'),
-    require(__DIR__ . '/../config/main-local.php')
+    require(__DIR__ . '/../config/main.php')
 );
 
 $application = new yii\web\Application($config);
