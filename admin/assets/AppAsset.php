cfbc1ebd4881c7a421d1a2da0ab8785f1f8e7e8a
layery <779487935@qq.com>
2016-12-12 19:18:54 +0800

    update

--- admin/assets/AppAsset.php
+++ admin/assets/AppAsset.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace frontend\assets;
+namespace admin\assets;
 
 use yii\web\AssetBundle;
 
@@ -12,9 +12,13 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        '/themes/default/easyui.css',
+        '/themes/icon.css',
+        '/themes/demo.css'
     ];
     public $js = [
+        'js/easyui/jquery.min.js',
+        'js/easyui/jquery.easyui.min.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
--- admin/web/index.php
+++ admin/web/index.php
@@ -4,6 +4,18 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 define('IS_AJAX', isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest');
 define('JS_URL', '/web/js');
 
+function p($data,$status=NULL){
+    echo '<pre>';
+    if(($data==NULL) || $status)
+        var_dump($data);
+    else
+        print_r($data);
+    die;
+    echo '</pre>';
+}
+
+
+
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../../common/config/bootstrap.php');
