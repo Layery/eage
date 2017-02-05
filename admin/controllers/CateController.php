8151b390649cbb96c3e01d7b8168cb8bd22c15b1
layery <779487935@qq.com>
2017-02-05 23:46:55 +0800

    测试

--- admin/controllers/CateController.php
+++ admin/controllers/CateController.php
@@ -47,3 +47,25 @@ class CateController extends BaseController
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- frontend/controllers/UserController.php
+++ frontend/controllers/UserController.php
@@ -1,51 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * Date: 2016/11/18
- * Time: 14:52
- */
 
-namespace frontend\controllers;
+namespace frontend\Controllers;
 
-use common\models\AgentTeam;
-use Yii;
-use yii\base\InvalidParamException;
-use yii\db\Query;
-use yii\helpers\ArrayHelper;
-use yii\web\BadRequestHttpException;
-use yii\web\Controller;
-use yii\filters\VerbFilter;
-use yii\filters\AccessControl;
-use yii\data\ArrayDataProvider;
-use common\models\LoginForm;
-use frontend\models\PasswordResetRequestForm;
-use frontend\models\ResetPasswordForm;
-use frontend\models\SignupForm;
-use frontend\models\ContactForm;
-
-class UserController extends Controller
+class UserController extends \yii\web\Controller
 {
-    /**
-     * @inheritdoc
-     */
-    public function actions()
+    public function actionIndex()
     {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
-    }
-
-
-    public function actionView()
-    {
-        $id = Yii::$app->getRequest()->get('id');
-        return $this->renderContent($id);
+        return $this->render('index');
     }
 
 }
