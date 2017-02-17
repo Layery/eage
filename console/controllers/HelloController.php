8d0394976872c5b632310bbf64c600c5559429ed
layery <779487935@qq.com>
2017-02-17 19:14:27 +0800

    拉代码

---
+++ console/controllers/HelloController.php
@@ -0,0 +1,44 @@
+<?php
+namespace console\controllers;
+
+use yii\console\Controller;
+
+class HelloController extends Controller
+{
+	public function actionIndex($message = 'Hello World')
+    {
+        echo $message . "\n";
+    }
+
+    public function actionDate()
+    {
+        $i = 0;
+        while (true)
+        {
+            if ($i == 10) break;
+            if ($i == 4) {
+				echo '哈哈哈' . "\n";
+			} else {		
+                echo date('Y-m-d H:m:s', time()) . "\n";
+			}
+            $i += 1;
+            sleep(1);
+        }
+    }
+    
+	public function actionTest($first, $second = 10)
+	{
+		for ($i = 1; $i <= $first; $i++) 
+		{
+	       for ($j = 1; $j <= $second; $j++) 
+			{
+				echo $i . 'x' . $j . '=' . $i*$j . "\n";
+                sleep(1);
+			}
+		}
+	}
+}
+
+
+
+
