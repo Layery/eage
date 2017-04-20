55f75fd78ee1ecce4e6cf9abdeba381fe1fb5d3e
unknown <liulongfei@LIULONGFEI.sogou-inc.com>
2017-04-20 09:57:56 +0800

    update

--- admin/views/auth/_add.php
+++ admin/views/auth/_add.php
@@ -14,54 +14,52 @@ use yii\helpers\Url;
 
 <div class="am-tabs am-margin" data-am-tabs>
     <ul class="am-tabs-nav am-nav am-nav-tabs">
-        <li class="am-active"><a href="#tab1">基本信息</a></li>
-        <li><a href="#tab2">选择权限</a></li>
+        <li class="am-active"><a href="#tab1">权限信息</a></li>
+
     </ul>
 
     <div class="am-tabs-bd">
         <div class="am-tab-panel am-fade am-in am-active" id="tab1">
             <form class="am-form" action="<?= Url::toRoute('auth/create');?>" method="post" id="am-form-1">
             <div class="am-g am-margin-top">
-                <div class="am-u-sm-4 am-u-md-2 am-text-right">角色名称</div>
+                <div class="am-u-sm-4 am-u-md-2 am-text-right">作用域范围</div>
                 <div class="am-u-sm-8 am-u-md-10">
-                    <input type="text" class="am-input-sm" name="name">
+                    <input type="text" class="am-input-sm" name="controller">
                 </div>
             </div>
 
             <div class="am-g am-margin-top">
-                <div class="am-u-sm-4 am-u-md-2 am-text-right">显示状态</div>
+                <div class="am-u-sm-4 am-u-md-2 am-text-right">动作范围</div>
                 <div class="am-u-sm-8 am-u-md-10">
-                    <div class="am-btn-group" data-am-button>
-                        <label class="am-btn am-btn-primary am-btn-xs">
-                            <input type="radio" name="status" id="option1" checked="checked" value="1"> 正常
-                        </label>
-                        <label class="am-btn am-btn-primary am-btn-xs">
-                            <input type="radio" name="status" id="option2" value="0"> 禁用
-                        </label>
-                    </div>
+                    <input type="text" class="am-input-sm" name="action">
                 </div>
-            </div>
-<!--            </form>-->
-        </div>
 
-        <div class="am-tab-panel am-fade" id="tab2">
-<!--            <form class="am-form" action="--><?//= Url::toRoute('auth/create');?><!--" method="post" id="am-form-2">-->
-                <?php foreach ($authAction->data as $controller => $row):?>
+            </div>
                 <div class="am-g am-margin-top">
-                    <div class="am-u-sm-4 am-u-md-2 am-text-right">
-                         <?= $row['label'];?>
-                    </div>
-                    <div class="am-btn-group" data-am-button>
-                           <?php foreach ($row['option'] as $action => $row):?>
-                               <label class="am-btn am-btn-primary am-btn-xs">
-                                   <input type="checkbox" name="actions[<?= $controller ?>][]" value="<?= $action;?>"> <?= $row['label'] ?> <span></span>
-                               </label>
-                           <?php endforeach;?>
+                    <div class="am-u-sm-4 am-u-md-2 am-text-right">是否禁用</div>
+                    <div class="am-btn-group" data-am-checkbox>
+                        <input type="checkbox" name="" value="">
                     </div>
                 </div>
-                <?php endforeach;?>
             </form>
         </div>
+
+<!--       <!-- <div class="am-tab-panel am-fade" id="tab2">-->
+<!--                --><?php /*//foreach ($authAction->data as $controller => $row):*/?>
+<!--                <div class="am-g am-margin-top">-->
+<!--                    <div class="am-u-sm-4 am-u-md-2 am-text-right">-->
+<!--                         --><?/*//= $row['label'];*/?>
+<!--                    </div>-->
+<!--                    <div class="am-btn-group" data-am-button>-->
+<!--                           --><?php /*//foreach ($row['option'] as $action => $row):*/?>
+<!--                               <label class="am-btn am-btn-primary am-btn-xs">-->
+<!--                                   <input type="checkbox" name="actions[--><?/*//= $controller */?><!--][]" value="--><?/*//= $action;*/?><!--"> --><?/*//= $row['label'] */?><!-- <span></span>-->
+<!--                               </label>-->
+<!--                           --><?php /*//endforeach;*/?>
+<!--                    </div>-->
+<!--                </div>-->
+<!--                --><?php /*//endforeach;*/?>
+<!--        </div>-->
     </div>
 </div>
 
