5374afc89ff5d76cb0e9f43ed25df8292067136b
layery <779487935@qq.com>
2017-04-24 00:42:28 +0800

    update Ace

--- admin/views/auth/list.php
+++ admin/views/auth/list.php
@@ -12,6 +12,7 @@ use yii\helpers\Url;
 use yii\grid\GridView;
 use common\widgets\Alert;
 use common\models\User;
+
 $this->title = 'AuthManage';
 $this->params['breadcrumbs'][] = $this->title;
 $Bright = yii::$app->right;
@@ -19,73 +20,126 @@ $user = User::model();
 
 ?>
 
-
-
-<div class="admin-content-body">
-    <div class="am-g">
-        <div class="am-u-sm-12 am-u-md-6">
-            <div class="am-btn-toolbar">
-                <div class="am-btn-group am-btn-group-xs">
-                    <button type="button" class="am-btn am-btn-default" id="am-btn-create"><span class="am-icon-plus"></span>
-                        <?= Html::a('新增', ['auth/create']) ?>
-                    </button>
-                    <button type="button" class="am-btn am-btn-default" id="am-btn-delete"><span class="am-icon-delete"></span> 删除选中</button>
-                </div>
-            </div>
-        </div>
-        <div class="am-u-sm-12 am-u-md-3">
-            <div class="am-input-group am-input-group-sm">
-                <input type="text" class="am-form-field">
-                <span class="am-input-group-btn">
-                    <button class="am-btn am-btn-default" type="button">搜索</button>
-                </span>
-            </div>
-        </div>
+<div class="page-content">
+    <div class="page-header">
+       <div class="search">
+           <table>
+               <tr>
+                   <td>角色名称</td>
+                   <td>
+                       <input type="text" class="col-xs-12">
+                   </td>
+                   <td>角色名称</td>
+                   <td>
+                       <input type="text" class="col-xs-12">
+                   </td>
+               </tr>
+           </table>
+       </div>
     </div>
-    <div class="am-g">
-        <div class="am-u-sm-12">
-                <table class="am-table am-table-striped am-table-hover table-main">
-                    <tbody>
-                    <tr>
-                        <?php
-                        echo GridView::widget([
-                            'dataProvider' => $dataProvider,
-                            'columns' =>[
-                                [
-                                    'class'=>'yii\grid\CheckboxColumn',
-                                    'checkboxOptions' => function($model){
-                                        return ['class' => 'gridview-check'];
-                                    },
-                                ],
-                                'id',
-                                'name',
-                                [
-                                    'attribute' => 'status',
-                                    'value' => function ($model) { return $model->status == 1 ? '正常' : '禁用';}
-                                ],
-                                [
-                                    'attribute' => 'actions',
-                                    'value' => function($model) use($Bright){
-                                        return $Bright->authCode2Name($model->actions);
-                                    },
-                                ],
+    <div class="row">
+        <div class="col-xs-12">
+            <table id="sample-table-1" class="table table-striped table-bordered table-hover">
+                <thead>
+                <tr>
+                    <th class="center">
+                        <label>
+                            <input type="checkbox" class="ace" />
+                            <span class="lbl"></span>
+                        </label>
+                    </th>
+                    <th>Domain</th>
+                    <th>Price</th>
+                    <th class="hidden-480">Clicks</th>
 
-                                'execute_id',
-                                [
-                                    'attribute' => 'created_at',
-                                    'value' => function($model) { return date('Y-m-d H:m', $model->created_at);}
-                                ],
-                                [
-                                    'class' => 'yii\grid\ActionColumn',
-                                    'template' => '{update} {delete}',
-                                ]
-                            ],
-                            'layout' => '{items}  {pager}'
-                        ]);
-                        ?>
-                    </tr>
-                    </tbody>
-                </table>
+                    <th>
+                        <i class="icon-time bigger-110 hidden-480"></i>
+                        Update
+                    </th>
+                    <th class="hidden-480">Status</th>
+
+                    <th>
+                        操作项
+                    </th>
+                </tr>
+                </thead>
+
+                <tbody>
+                <tr>
+                    <td class="center">
+                        <label>
+                            <input type="checkbox" class="ace" />
+                            <span class="lbl"></span>
+                        </label>
+                    </td>
+
+                    <td>
+                        <a href="#">ace.com</a>
+                    </td>
+                    <td>$45</td>
+                    <td class="hidden-480">3,330</td>
+                    <td>Feb 12</td>
+
+                    <td class="hidden-480">
+                        <span class="label label-sm label-warning">Expiring</span>
+                    </td>
+
+                    <td>
+                        <div class="visible-md visible-lg hidden-sm hidden-xs btn-group">
+                            <button class="btn btn-xs btn-success">
+                                <i class="icon-ok bigger-120"></i>
+                            </button>
+
+                            <button class="btn btn-xs btn-info">
+                                <i class="icon-edit bigger-120"></i>
+                            </button>
+
+                            <button class="btn btn-xs btn-danger">
+                                <i class="icon-trash bigger-120"></i>
+                            </button>
+
+                            <button class="btn btn-xs btn-warning">
+                                <i class="icon-flag bigger-120"></i>
+                            </button>
+                        </div>
+
+                        <div class="visible-xs visible-sm hidden-md hidden-lg">
+                            <div class="inline position-relative">
+                                <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown">
+                                    <i class="icon-cog icon-only bigger-110"></i>
+                                </button>
+
+                                <ul class="dropdown-menu dropdown-only-icon dropdown-yellow pull-right dropdown-caret dropdown-close">
+                                    <li>
+                                        <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
+																				<span class="blue">
+																					<i class="icon-zoom-in bigger-120"></i>
+																				</span>
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
+																				<span class="green">
+																					<i class="icon-edit bigger-120"></i>
+																				</span>
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
+																				<span class="red">
+																					<i class="icon-trash bigger-120"></i>
+																				</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
