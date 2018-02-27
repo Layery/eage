9e27026445b246f80b39d88a3711fb3fb11c3b03
unknown <liulongfei@LIULONGFEI.sogou-inc.com>
2018-02-27 18:54:32 +0800

    add: add redis console

---
+++ console/controllers/RedisController.php
@@ -0,0 +1,15 @@
+<?php
+namespace console\controllers;
+
+use yii;
+use yii\redis\cache;
+use yii\console\Controller;
+
+
+class RedisController extends Controller
+{
+    public function actionIndex()
+    {
+
+    }
+}
